215318c69e5b27171a1179972686b875d1b9ecca
Harry <Harry_emmett@outlook.com>
2022-01-07 15:15:57 +0000

    added more CRUD

--- routes/beerRoutes.js
+++ routes/beerRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const res = require('express/lib/response');
 // const { request } = require('express');
 const { Beer } = require('../model/beer.js');
 
@@ -28,7 +29,26 @@ router.get('/read/:id', (request, response) => {
 
 //update
 router.put('/update/:id', (request, response) => {
-    
+    const body = request.body;
+    const id = request.params.id;
+    console.log(request.params.id);
+    response.status(201).send(`${id} has been updated`);
+   
+   
+    //const beer = new Beer(body);
+
+    // beer.save().then((result) => {
+    //     response.status(201).send(`${result.beerName} updated`);
+    // });
+
+    // beer.findByIdAndUpdate(request.params.id, (error, beer) => {
+    //     response.status(201).send(beer + "has been updated");
+    // });   
+
+    // const beer = new Beer(request.body);
+    // beer.save().then((result) => {
+    //     response.status(201).send(`${result.beerName} has been added to the database`);
+    // });
 });
 
 //delete
